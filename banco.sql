7b3af6c90966b7fad2b7a088caa4952eab06f8be
Leandro Santana <35153858+Leandro-Santana-Andrade@users.noreply.github.com>
2024-11-02 21:27:52 -0300

    Update banco.sql
    Add campos na tabela

--- banco.sql
+++ banco.sql
@@ -1,8 +1,10 @@
 CREATE TABLE dados (
-    AlunoID int,
+    Matricula int not NULL,
     Nome varchar(50),
     Sobrenome varchar(50),
+    DataNascimento date,
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    Host varchar(50),
+    PRIMARY KEY (Matricula)
 );
